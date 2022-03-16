8953803054f21f7c834fcdea4c71752619de9ab9
nicolasvallejo13 <nicolas.vallejo.pabon@gmail.com>
2022-03-16 12:29:44 -0500

    second assigment

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,9 +1,51 @@
 import random
-minusculas = "abcdefghiklmn"
-mayusculas = "ABCDEFGHIJKL"
-numeros = "123456789"
-respuesta = ""
-app = input ("¿para qué APP desea usted la contraseña? ")
-for x in range(0,4):
-    respuesta = respuesta + random.choice(minusculas) + random.choice(mayusculas) + random.choice(numeros)
-print ( "su contraseña para  "+ app +" es " + respuesta)
+import sys
+class passwordGeneratorClass :
+    
+        
+    def __password_generator(self):
+        low_cases = "abcdefghiklmnopqrstuvwxyz"
+        upper_cases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        numbers = "123456789"
+        passwd = ""
+        for _ in range(0,4):
+            passwd = passwd + random.choice(low_cases) + random.choice(upper_cases) + random.choice(numbers )
+        return passwd
+
+
+    def __password_reader(self, required_password):
+        with open('stored_passwords.txt') as file:
+        
+            for line in file:
+                if line.split()[0].lower() == required_password.lower():
+                    print("the password for the " + required_password +" app is the following one: " + line.split()[1])
+                    return True
+        return False
+
+
+    def __password_writter(self, required_password):
+        with open('stored_passwords.txt' , "a") as file:
+            file.write('\n')
+            new_password = self.__password_generator()
+            file.write(required_password + " " + new_password)
+            print("the password for " + required_password + " will be " + new_password + " and it has been sotred correctly in the txt file ")
+
+
+    def __password_cheker(self, name_of_app):
+        if self.__password_reader(name_of_app):
+            print("Thank you for using this app")
+        else:
+            self.__password_writter(name_of_app)
+
+    def app_starter(self):
+        if (len(sys.argv) == 3 and sys.argv[1].lower() == "app".lower()):
+            self.__password_cheker(sys.argv[2])
+        else:
+            name_of_app = input("please type the name of the app ")
+            self.__password_cheker(name_of_app)
+
+paswordGeneratorApp = passwordGeneratorClass()
+paswordGeneratorApp.app_starter()
+
+
+
